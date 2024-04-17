c7bc5be680591c0a9f1cd7033f49496232868c3f
khawla_ben <refeser52@gmail.com>
2024-04-17 12:46:13 +0000

    updates

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,21 +3,19 @@
 save to json
 """
 
-import json
-import os.path
-import sys
-from sys import argv
+if __name__ == "__main__":
+	import sys
+	import json
+	save_to_json_file = \
+		__import__('7-save_to_json_file').save_to_json_file
+	load_from_json_file = \
+		__import__('8-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-filename = "add_item.json"
-json_list = []
-
-if os.path.exists(filename):
-    json_list = load_from_json_file(filename)
-
-for index in argv[1:]:
-    json_list.append(index)
-
-save_to_json_file(json_list, filename)
+	filename = "add_item.json"
+	with open(filename, 'a+') as f:
+		if f.tell() == 0:
+			json.dump([], f)
+	file_data = load_from_json_file("add_item.json")
+	if len(sys.argv) > 1:
+		file_data.extend(sys.argv[1:])
+	save_to_json_file(file_data, filename)
