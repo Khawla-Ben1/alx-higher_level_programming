671bec7c674b0fdc98424ccae9bd726e46f3caca
khawla_ben <refeser52@gmail.com>
2024-05-11 15:00:23 +0000

    updates

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 9):
-    for j in range (0, 10)
+    for j in range(0, 10)
         if j > i :
-            print("{} {}".format(i, j), end(', '))
+            print("{} {}".format(i, j), end=', ')
