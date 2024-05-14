fb392bfdaa4b9cf76abbde641de20b559a3d532b
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-05-14 17:17:15 +0100

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
-def add(a, b):
-    return (a + b)
+#!/usr/bin/python3
+from add_0 import add
 
 a = 1
 b = 2
 sum = add(a, b)
-print(f"{a} + {b} = {sum}", end='\n')
+print("{} + {} = {}".format(a, b, sum), end='\n')
