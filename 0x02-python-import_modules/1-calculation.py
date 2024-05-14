b842d182ca9d95d4859ccccb9a411c46f2c201fb
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-05-14 17:49:07 +0100

    1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, sum), end='\n')
-    print("{} - {} = {}".format(a, b, sub), end='\n')
-    print("{} * {} = {}".format(a, b, mul), end='\n')
-    print("{} / {} = {}".format(a, b, div), end='\n')
+    print("{} + {} = {}".format(a, b, sum(a, b)), end='\n')
+    print("{} - {} = {}".format(a, b, sub(a, b)), end='\n')
+    print("{} * {} = {}".format(a, b, mul(a, b)), end='\n')
+    print("{} / {} = {}".format(a, b, div(a, b)), end='\n')
