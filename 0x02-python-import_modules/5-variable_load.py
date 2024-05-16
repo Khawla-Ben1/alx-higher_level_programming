5280df3636e0e4aec0d48f41f15be8f63baf750b
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-05-16 13:46:41 +0100

    updates

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
+    """Print a defined in variable_load_5 module."""
     import variable_load_5
-print(a)
+    print(variable_load_5.a)
