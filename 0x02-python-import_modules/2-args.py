fb67ac8332ce4cca5bdbccbd224d056f097d55cd
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-05-16 13:18:14 +0100

    new updates

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,10 +7,9 @@ import sys
 num = len(sys.argv) - 1
 if num > 1:
     print("{} arguments:".format(num), end='\n')
-elif num == 1
+elif num == 1:
     print("1 argument:")
 else:
     print("{} arguments.".format(num), end='\n')
 for i in range(num):
     print("{}: {}".format(i + 1, sys.argv[i + 1]), end='\n')
-
