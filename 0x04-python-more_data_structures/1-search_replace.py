c7197ac21ffaaf35cb53f3f6bc239c9265cb6d5c
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-05-18 18:17:40 +0100

    updates

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in my_list:
-        if i == search:
-            i = replace
-    return my_list
+    new_list = [replace if x == search else x for x in my_list]
+    return new_list
--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique = set(my_list)
-    sum = sum(unique)
-    return sum
+    total = sum(unique)
+    return total
