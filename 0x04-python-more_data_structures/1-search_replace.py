2ce891ca78799390a764b1ceec5095b94ece3659
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-05-18 18:09:24 +0100

    updates

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     for i in my_list:
-        if my_list[i] == search:
-            my_list[i] = replace
+        if i == search:
+            i = replace
     return my_list
