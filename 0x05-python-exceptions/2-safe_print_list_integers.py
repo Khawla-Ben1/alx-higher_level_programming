16eda2b26604f0d86f83b6ff59c2b728dc084f33
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-05-19 18:02:57 +0100

    updates

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,8 +9,6 @@ def safe_print_list_integers(my_list=[], x=0):
             printed_count += 1
         except (ValueError, TypeError):
             pass
-        except IndexError:
-            break
         count += 1
     print()
     return printed_count
