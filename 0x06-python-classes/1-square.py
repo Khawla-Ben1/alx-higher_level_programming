546dcffe4c6d3130cb7e6498c41e275b8501fc57
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-05-19 18:46:13 +0100

    updates

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Module 1-square
+Defines a square class with a private instance attribute.
+"""
+
+class Square:
+    """
+    Defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
