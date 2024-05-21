120b580be354981457094f5f77e658e4a18e654e
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-05-21 11:40:33 +0100

    new updates

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
--- This script uses the CREATE DATABASE statement to create the hbtn_0c_0
--- database, but it includes the IF NOT EXISTS clause to prevent any 
--- errors if the database already exists.
+-- Create database if missing
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
