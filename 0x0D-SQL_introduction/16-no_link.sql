62b51bc1fb6807fb97ff1ed5b4756d87de21962f
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-05-21 11:31:30 +0100

    new updates

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 --no_link
-SELECT name, score FROM second_table WHERE name IS NOT NULL AND name != ''
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != ''
 ORDER BY score DESC;
