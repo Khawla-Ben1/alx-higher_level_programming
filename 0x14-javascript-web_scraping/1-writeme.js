435318c6cfc3dc47de8b385f8a48dadce3a8a4b9
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-08-04 01:46:36 +0100

    push

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,14 +3,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
-// if (!filePath) {
-//   console.error('Error: No file path provided.');
-//   process.exit(1);
-// }
 fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
   if (err) {
     console.error(err);
-    return;
   }
-//   console.log(data);
 });
