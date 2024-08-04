693dac1e0add1f3ed4a11a7d73defec98420bf59
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-08-04 02:25:58 +0100

    push

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,5 +4,9 @@ const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
 
 re.get(url, { json: true }, (error, response, body) => {
+  if (error) {
+  console.error('Error:', error.message);
+  return;
+  }
   console.log(body.title);
 });
