e673c1df6c6bb384564df535bff9cc18e036983b
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-08-04 03:46:45 +0100

    push

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,20 +3,23 @@ const request = require('request');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
 
-request(url,function (error, response, body) {
-  if (error) {
-    console.error('Error:', error.message);
+function helpRequest (arr, i) {
+  if (i === arr.length) {
     return;
   }
-  const charactersUrls = JSON.parse(body.characters);
-  if (i === charactersUrls.length) {
-    return;
-  }
-  request(charactersUrls[i], function (error, response, body) {
+  request(arr[i], function (error, response, body) {
     if (error) {
       console.error(error);
     }
     console.log(JSON.parse(body).name);
-    helpRequest(charactersUrls, i + 1);
+    helpRequest(arr, i + 1);
   });
+}
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const c = JSON.parse(body).characters;
+  helpRequest(c, 0);
 });
