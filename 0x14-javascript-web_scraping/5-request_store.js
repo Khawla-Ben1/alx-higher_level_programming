2acda26f09d611f45ac6a7729c30a7e71193734b
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-08-04 03:18:38 +0100

    push

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const filePath = process.argv[3];
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+      // if (err) {
+      //   console.error('Error writing file:', err);
+      // } else {
+      //   console.log('File saved successfully');
+      // }
+    });
+});
