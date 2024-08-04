4a0c6235d8a1f08bc74682150f7b266809584170
KHAWLA BEN HAMMOU <115346619+Khawla-ben@users.noreply.github.com>
2024-08-04 01:40:18 +0100

    0x14-javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 if (!filePath) {
-    console.error("Error: No file path provided.");
+    console.error('Error: No file path provided.');
     process.exit(1);
 }
 fs.readFile(filePath, 'utf8', (err, data) => {
