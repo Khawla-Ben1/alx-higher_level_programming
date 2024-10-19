434b4079739476a2801f9ad50b69edadf4a522f4
Khawla Ben hammou <kben-ham@e2r2p2.1337.ma>
2024-10-19 15:54:37 +0100

    updates

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -9,25 +9,28 @@
  */
 void print_python_string(PyObject *p)
 {
+    PyObject *str, *repr;
 
-	PyObject *str, *repr;
+    printf("[.] string object info\n");
 
-	(void)repr;
-	printf("[.] string object info\n");
-
-	if (strcmp(p->ob_type->tp_name, "str"))
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
-
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-
-	repr = PyObject_Repr(p);
-	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
-	printf("  value: %s\n", PyBytes_AsString(str));
+    if (!PyUnicode_Check(p))
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+        printf("  type: compact ascii\n");
+    else
+        printf("  type: compact unicode object\n");
+    printf("  length: %ld\n", PyUnicode_GET_LENGTH(p));
+    repr = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
+    if (repr != NULL)
+    {
+        printf("  value: %s\n", PyBytes_AsString(repr));
+        Py_DECREF(repr);
+    }
+    else
+    {
+        printf("  [ERROR] Unable to encode string\n");
+    }
 }
