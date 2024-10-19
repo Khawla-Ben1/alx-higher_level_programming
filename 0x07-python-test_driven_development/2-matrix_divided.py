9a4132d06691c4dd718ae5f84e5928e949add808
Khawla Ben hammou <kben-ham@e2r2p2.1337.ma>
2024-10-19 15:45:13 +0100

    updates

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python3
+"""Matrix division"""
+
+
 def matrix_divided(matrix, div):
-    if not isinstance(matrix, list) or any(not isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    if any(len(row) != len(matrix[0]) for row in matrix):   
+    """
+    Args: Divides all element of a matrix.
+        :param matrix: must be a list of lists of integers or floats.
+        :param div: a number (integer or float), can't be equal to 0,
+                    used in dividing all elements of the matrix,
+                    rounded to 2 decimal places.
+    Returns: new matrix.
+    """
+    import decimal
+    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) is not list:
+        raise TypeError(error_msg)
+    len_rows = []
+    row_count = 0
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError(error_msg)
+        len_rows.append(len(row))
+        for element in row:
+            if type(element) not in [int, float]:
+                raise TypeError(error_msg)
+        row_count += 1
+    if len(set(len_rows)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
-    if not isinstance(div, (int, float)):
+    if type(div) not in [int, float]:
         raise TypeError("div must be a number")
-    if div == 0:
+    if int(div) == 0:
         raise ZeroDivisionError("division by zero")
-    try:
-        return [[round(element / div, 2) for element in row] for row in matrix]
-    except TypeError:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    new_matrix = list(map(lambda row:
+                          list(map(lambda x: round(x/div, 2), row)), matrix))
+    return new_matrix
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
-"""Module for text_indentation method."""
+"""Text indentation"""
 
 
 def text_indentation(text):
-    """Method for adding 2 new lines after '.?:' characters."""
+    """
+    Args: Prints a text with 2 new lines after each of these
+    character . ? and :
+    There should be no space at the beginning or at the end of
+    each printed line.
+
+        :param text: the text to print (a string).
+
+    Raises: TypeError - If text is not a string.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for delim in ".?:":
-        text = (delim + "\n\n").join(
-            [line.strip(" ") for line in text.split(delim)])
+    count = 0
+    while count < len(text) and text[count] == " ":
+        count = count + 1
 
-    print(text, end="")
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+    while count < len(text):
+        print(text[count], end="")
+        if text[count] == "\n" or text[count] in ".?:":
+            if text[count] in ".?:":
+                print("\n")
+            count = count + 1
+            while count < len(text) and text[count] == " ":
+                count = count + 1
+            continue
+        count = count + 1
