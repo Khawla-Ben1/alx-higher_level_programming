170ee036fd706a31c5fadd9c6507e2ea9d968f2f
Khawla Ben hammou <kben-ham@e2r2p2.1337.ma>
2024-10-19 15:56:48 +0100

    updates

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -4,18 +4,15 @@
 
 def print_square(size):
     """
-    Args: Prints a square with the character '#'
-        :param size: length of the square (must be and integer).
-
-    Raises:
-        TypeError - if size is not an integer, is a float and < 0,
-        ValueError - if size < 0.
+    Args: Prints a square with the character 'x'
+        :param size: length of the square (must be an integer).
     """
-    if type(size) != int:
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    
     for i in range(size):
         for j in range(size):
-            print("#", end='')
+            print("x", end='')
         print()
